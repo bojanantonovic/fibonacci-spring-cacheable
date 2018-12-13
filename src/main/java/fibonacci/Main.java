f7cf7827d382628261d0724dfde6e10d0c2c0c43
Bojan Antonovic <bojan@antonovic.ch>
2018-12-13 01:01:28 +0100

    modernization

--- src/main/java/fibonacci/Main.java
+++ src/main/java/fibonacci/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String... args) throws Exception {
         try (var context = new AnnotationConfigApplicationContext(CacheConfiguration.class, FibonacciService.class)) {
             var service = context.getBean(FibonacciService.class);
 
